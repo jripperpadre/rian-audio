fe5288a6640ce2b6d2b95ef5a5e306ad60dbad52
John <jripperpadre@gmail.com>
2026-07-08 03:33:05 +0300

    new updates on User Interfaces

--- rian_backend/settings.py
+++ rian_backend/settings.py
@@ -333,5 +333,12 @@ JAZZMIN_UI_TWEAKS = {
 # ------------------------------
 CART_SESSION_ID = "cart"
 
+# ------------------------------
+# Auth redirects
+# ------------------------------
+LOGIN_URL = "/login/"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
 
 
